b70f54c6141d5ae0b61e42271ab6630137665892
justyuri4 <101870859+justyuri4@users.noreply.github.com>
2025-10-23 16:05:13 +0900

    fix whisper,add whisper-base-mlx

--- whisper_interp.py
+++ whisper_interp.py
@@ -1,16 +1,26 @@
 import mlx_whisper
 
 class interp:
-    def preprocess_audio() -> str:
+    def preprocess_audio(self) -> str:
+        """
+        指定されたWAVファイルをmlx-whisperを使用して文字起こしする。
+
+        処理の結果を標準出力に出力する。
+
+        Returns:
+            str: 文字起こしされたテキスト。
+        """
+
         audio_file_path = "python-audio-output.wav"
 
         result = mlx_whisper.transcribe(
         audio_file_path, path_or_hf_repo="whisper-base-mlx"
         )
-        print(result)
-    '''
-    :param none
-    :return 音声ファイルを文字起こししたテキストを返す
-        音声ファイルをmlx_whisperを使って文字起こしする処理
-    '''
-       
+        return(result)
+    
+
+if __name__ == "__main__":
+    instance = interp()
+    text = instance.preprocess_audio()
+    print(text)
+
