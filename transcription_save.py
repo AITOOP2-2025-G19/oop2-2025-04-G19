84d2ed09052194ae266cfc71a471cd6435be8069
Haruto-Koshimizu <ko.haru414@icloud.com>
2025-10-23 15:42:07 +0900

    Update transcription_save.py

--- transcription_save.py
+++ transcription_save.py
@@ -8,5 +8,6 @@ def transcription_save (text) -> None:
         with open('transcription','a') as f:
             writer = csv.writer(f)
             csv_writer.writerow(row_data)
+        print(f"'{text}' を {FILE_NAME} に書き出しました。 ")
     except Exception as e:
         print(f" 書き込み中にエラーが発生しました: {e}")
